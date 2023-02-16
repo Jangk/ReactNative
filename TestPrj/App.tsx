3d1a237c35984d497687adfbcef2f3fac869dbe0
장재완 <wkdwodhks7@naver.com>
2023-02-16 17:32:51 +0900

    style

--- TestPrj/App.tsx
+++ TestPrj/App.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Text, SafeAreaView, ScrollView } from 'react-native';
+import { Text, SafeAreaView, Alert, TextInput, StyleSheet } from 'react-native';
 import * as M from './data'
-import type { myModule } from './data/MyModule'
 
 
 const module = M.CreateMyModule();
 const arrM = Array(100).map(M.CreateMyModule);
+const styles = StyleSheet.create({
+  SafeAreaView: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  text: { fontSize: 20 }
+})
 
+
+const onPress = () => Alert.alert('Haed', 'Body')
 
 export default function App() {
   console.log('App called')
   return (
-    <SafeAreaView>
-      <ScrollView><Text>{JSON.stringify(module, null, 2)}</Text></ScrollView>
+    <SafeAreaView style={[styles.SafeAreaView, { backgroundColor: 'grey' }]}>
+      <TextInput
+        placeholder="기본 입력 값"
+        onChangeText={(text: string) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('onEndEditing')}
+      />
     </SafeAreaView>
   )
 }
+
