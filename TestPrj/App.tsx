1361e3bdf1782274a1ced4e6d6440c4e8b03a28d
장재완 <wkdwodhks7@naver.com>
2023-02-17 16:41:23 +0900

    스타일

--- TestPrj/App.tsx
+++ TestPrj/App.tsx
@@ -1,30 +1,36 @@
 import React from 'react';
-import { Text, SafeAreaView, Alert, TextInput, StyleSheet } from 'react-native';
-import * as M from './data'
+import {
+  Text, View, SafeAreaView, Alert, TextInput, StyleSheet, Platform, Dimensions,
+  Image, ImageBackground
+} from 'react-native';
+import { MD2Colors, MD3Colors } from 'react-native-paper'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import * as M from './src/data'
+import TopBar from './src/screens/TopBar'
+import Content from './src/screens/Content'
+import BottomBar from './src/screens/BottomBar'
 
 
-const module = M.CreateMyModule();
-const arrM = Array(100).map(M.CreateMyModule);
+
+// 변수
 const styles = StyleSheet.create({
-  SafeAreaView: { flex: 1, alignItems: 'center', justifyContent: 'center' },
-  text: { fontSize: 20 }
+  SafeAreaView: { flex: 1, padding: 10 },
+  text: { fontSize: 20, color: MD2Colors.blue500, marginBottom: Platform.select({ ios: 0, android: 20 }) },
+  box: { height: 100, backgroundColor: MD2Colors.white, marginBottom: 10 },
+  border: { boderWidth: 10, borderColor: MD2Colors.lime500 },
+  ImageBackground: { padding: 10 },
+  Image: { width: 50, height: 50, borderRadius: 10 },
+  Bold: { fontFamily: 'DancingScript-Bold', textAlign: 'center', fontSize: 20 }
 })
 
-
-const onPress = () => Alert.alert('Haed', 'Body')
-
+// 함수
 export default function App() {
-  console.log('App called')
+  console.log('App Called')
   return (
-    <SafeAreaView style={[styles.SafeAreaView, { backgroundColor: 'grey' }]}>
-      <TextInput
-        placeholder="기본 입력 값"
-        onChangeText={(text: string) => console.log(text)}
-        onFocus={() => console.log('onFocus')}
-        onBlur={() => console.log('onBlur')}
-        onEndEditing={() => console.log('onEndEditing')}
-      />
+    <SafeAreaView style={styles.SafeAreaView}>
+      <TopBar />
+      <Content />
+      <BottomBar />
     </SafeAreaView>
   )
 }
-
